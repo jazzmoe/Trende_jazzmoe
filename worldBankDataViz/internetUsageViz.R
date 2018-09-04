776c28389f5afcc28dbfebd7e114d048c80b7ce1
Trende <alexander.trende@uni-oldenburg.de>
2018-09-04 21:05:12 +0200

    plotted internet usage depending on continent

--- worldBankDataViz/internetUsageViz.R
+++ worldBankDataViz/internetUsageViz.R
@@ -4,11 +4,17 @@ setwd("C:/Users/User/Code/R/trende_jazzmoe/Trende_jazzmoe/worldBankDataViz")
 dataset <- read_excel("dataInternetUsage.xls")
 
 # find the row of a certain country. e.g. Germany
-germany = as.double(dataset[which(dataset$`Data Source` == "Germany"), 5:62])
-ghana = as.double(dataset[which(dataset$`Data Source` == "Ghana"), 5:62])
-world = as.double(dataset[which(dataset$`Data Source` == "World"), 5:62])
 years = as.double(dataset[3,5:62])
-df = data.frame(years, germany, ghana, world)
+
+germany = as.double(dataset[which(dataset$`Data Source` == "Germany"), 5:62])
+eu = as.double(dataset[which(dataset$`Data Source` == "European Union"), 5:62])
+north_america = as.double(dataset[which(dataset$`Data Source` == "North America"), 5:62])
+sub_saharan_africa = as.double(dataset[which(dataset$`Data Source` == "Sub-Saharan Africa"), 5:62])
+south_asia = as.double(dataset[which(dataset$`Data Source` == "South Asia"), 5:62])
+latin_america = as.double(dataset[which(dataset$`Data Source` == "Latin America"), 5:62])
+world = as.double(dataset[which(dataset$`Data Source` == "World"), 5:62])
+
+df = data.frame(years, germany, eu, north_america, latin_america, sub_saharan_africa, sout_asia, world)
 
 
 ## ggPlot magic
