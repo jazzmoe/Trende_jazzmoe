86e8c4fdae5cea821fcc33e48976b799acd2e632
Trende <alexander.trende@uni-oldenburg.de>
2018-09-04 20:38:50 +0200

    good start

--- worldBankDataViz/internetUsageViz.R
+++ worldBankDataViz/internetUsageViz.R
@@ -4,10 +4,23 @@ setwd("C:/Users/User/Code/R/trende_jazzmoe/Trende_jazzmoe/worldBankDataViz")
 dataset <- read_excel("dataInternetUsage.xls")
 
 # find the row of a certain country. e.g. Germany
-idxGermany = which(dataset$`Data Source` == "Germany")
-dataGermany = dataset[idxGermany, 5:62]
-plot(as.double(dataChar))
+germany = as.double(dataset[which(dataset$`Data Source` == "Germany"), 5:62])
+ghana = as.double(dataset[which(dataset$`Data Source` == "Ghana"), 5:62])
+world = as.double(dataset[which(dataset$`Data Source` == "World"), 5:62])
+years = as.double(dataset[3,5:62])
+df = data.frame(years, germany, ghana, world)
 
 
 ## ggPlot magic
 library(ggplot2)
+d <- melt(df, id.vars="years")
+
+# Everything on the same plot
+ggplot(d, aes(years,value, col=variable)) + 
+  geom_point() + 
+  stat_smooth() +
+  xlim(1989, 2017) +
+  xlab("Year") + 
+  ylab("Percentage of population") + 
+  ggtitle("Internet usage [%]")
+
