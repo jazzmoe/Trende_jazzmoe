fa931fc200ff26adfbca625522518818e001618f
jazzmoe <mhemmerlein@gmx.de>
2018-11-08 18:51:40 -0600

    acc: 82

--- kaggle-challenges/titanic-survivers/00-script.R
+++ kaggle-challenges/titanic-survivers/00-script.R
@@ -39,46 +39,57 @@ SubTemp <- read_csv("./data/gender_submission.csv")
 
 ### Data prep
 names(Train) <- str_to_lower(names(Train))
+names(Test) <- str_to_lower(names(Test))
 Train <- Train %>% mutate(survived = as.factor(survived))
 glimpse(Train)
 
 # define test and training in test set
 TrainTest <- sample_n(Train, size = round(0.2*(nrow(Train))))
-Train <- Train[!(Train$passengerid %in% TrainTest$passengerid),]
+TrainTrain <- Train[!(Train$passengerid %in% TrainTest$passengerid),]
 
-# EDA: exploratory data analysis
+#### EDA: exploratory data analysis
+Train %>% ggplot() + geom_histogram(aes(x = age))
+Train %>% ggplot() + geom_histogram(aes(x = fare))
+Train %>% ggplot() + geom_bar(aes(x = sex))
+Train %>% ggplot() + geom_bar(aes(x = pclass))
+
+table(Train$sibsp, Train$survived)
+chisq.test(Train$sibsp, Train$survived) # significant relationship
 
 ### Simple logistic regression
-Mod11 <- glm(survived ~ sex + fare, data = Train, family = "binomial", na.action = na.omit)
+Mod1 <- glm(survived ~ sex + fare, data = TrainTrain, family = "binomial", na.action = na.omit)
 TrainTest <- TrainTest %>% mutate(
-  surv.mod1 = ifelse(predict(Mod1, newdata = TrainTest, type = "response") > 0.5, 1, 0),
+  surv.mod1 = ifelse(predict(Mod1, newdata = TrainTest, type = "response") > 0.6, 1, 0),
   surv.mod1 = ifelse(is.na(surv.mod1), 0, identity(surv.mod1)))
 accuracy(TrainTest$survived, TrainTest$surv.mod1)
 
 # alternative / full model
-Mod11.1 <- glm(survived ~ sex + fare + pclass + age + parch + sibsp, data = Train, family = "binomial")
-summary(Mod1.1)
+Mod1.1 <- glm(survived ~ sex + fare + pclass + age, data = TrainTrain, family = "binomial")
 TrainTest <- TrainTest %>% mutate(
-  surv.mod1.1 = ifelse(predict(Mod1.1, newdata = TrainTest, type = "response") > 0.5, 1, 0),
-  surv.mod1 = ifelse(is.na(surv.mod1), 0, identity(surv.mod1)))
+  surv.mod1.1 = ifelse(predict(Mod1.1, newdata = TrainTest, type = "response") > 0.6, 1, 0),
+  surv.mod1.1 = ifelse(is.na(surv.mod1), 0, identity(surv.mod1)))
 accuracy(TrainTest$survived, TrainTest$surv.mod1.1)
 
-# compare models
-lrtest(Mod1, Mod11.1) # probably missings in Mod1.1
-
-
 ### random effects model
-Mod2 <- glmer(survived ~ sex + fare + sibsp + (1 | pclass), data = Train, family = binomial)
+Mod2 <- glmer(survived ~ sex + fare + sibsp + (1 | pclass), data = TrainTrain, family = binomial)
 summary(Mod2)
-Train <- Train %>% mutate(surv.mod2 = ifelse(predict(Mod2, type = "response") > 0.6, 1, 0))
-accuracy(Train$survived, Train$surv.mod2)
+TrainTest <- TrainTest %>% mutate(
+  surv.mod2 = ifelse(predict(Mod2, newdata = TrainTest, type = "response") > 0.6, 1, 0))
+accuracy(TrainTest$survived, TrainTest$surv.mod2)
 
 # good source https://www.r-bloggers.com/evaluating-logistic-regression-models/
 ### machine learning logit (caret)
 trainData <- trainControl(method = "boot", number = 10, savePredictions = TRUE)
-Mod3 <- train(survived ~ sex + fare + pclass + age + parch + sibsp, 
-              data = Train, trControl = trainData, method = "glm", family = "binomial",
+Mod3 <- train(survived ~ sex + pclass + age + sibsp + parch + fare, 
+              data = Train, trControl = trainData, method = "rf", nTree = 100,
               metric = "Accuracy", na.action = na.omit)
-Mod3$pred
-summary(Mod3)
+confusionMatrix(Mod3)
 print(Mod3)
+accuracy(Mod3$pred$pred, Mod3$pred$obs)
+Mod3.pred <- ifelse(predict(Mod3, newdata = Test, type = "prob") > 0.5, 1, 0) %>% 
+  as_tibble() %>% rename("Died" = `0`, "Survived" = `1`)
+
+
+### create forecast
+Bench <- cbind(SubTemp, modpred = Mod3.pred$Survived)
+table(Bench$Survived, Bench$modpred)
